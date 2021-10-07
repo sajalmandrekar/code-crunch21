2d243a5fb8b747279ad4a9640171154372243a7b
Sajal Mandrekar <sajalmandrekar@gmail.com>
2021-10-07 23:01:08 +0530

    main updated 404 error

--- main.py
+++ main.py
@@ -1,102 +1,107 @@
-from os import error
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status, HTTPException
 from datetime import datetime, timedelta
+from fastapi.responses import JSONResponse
 import requests
 from calendar import monthrange
+from pydantic import BaseModel
+import os
 
-'''
-    API_KEY = '77Pj5RyutTSatUVfn210VEkvOa3DOk7GDyiCDL1m'
-    GET {BASE_URL}planetary/apod
-'''
-API_KEY = '77Pj5RyutTSatUVfn210VEkvOa3DOk7GDyiCDL1m'
+from starlette.exceptions import HTTPException as StarletteHTTPException
+
+API_KEY = os.environ.get('NASA_KEY')
 BASE_URL = 'https://api.nasa.gov/'
 
 def number_of_days_in_month(year, month):
+    '''
+        This function returns the number of days of a given month in an year.
+        eg. (2021, 10) returns 31 because Oct 2021 has 31 days.
+    '''
     return monthrange(year, month)[1]
 
-error_output = {
-  "status": 404,
-  "message": "image/video not found"
-}
+class ExceptionModel(BaseModel):
+    status: int
+    message: str
 
-app = FastAPI()
+def raiseException():
+    raise HTTPException(status_code=404, detail={
+        "status": 404,
+        "message": "image/video not found"
+    })
 
-@app.get('/nasa/image-of-month')
-async def get_apod():
+
+app = FastAPI()     # the base app
+
+
+@app.exception_handler(StarletteHTTPException)
+async def http_exception_handler(request, exc):
+    return JSONResponse(exc.detail, status_code=exc.status_code)
+
+
+@app.get('/nasa/image-of-month', responses={404: {"model": ExceptionModel}})
+async def get_apod(response: Response):
     now=datetime.now()
     date = datetime(now.year, now.month, 1).date()
     URL = f'{BASE_URL}planetary/apod/?date={date}&&api_key={API_KEY}'
-    response = requests.get(URL,verify=False)
+    response = requests.get(URL)
     result = response.json()
     try:
         result = {key: result[key] for key in ['date', 'media_type', 'title', 'url']}
     except:
-        result = response.json()
-    return error_output,404
+        raiseException()
+    return result
 
-@app.get('/nasa/images-of-month/{year}/{month}')
-async def get_apod(year :int, month: str):
+@app.get('/nasa/images-of-month/{year}/{month}', responses={404: {"model": ExceptionModel}})
+async def get_apod(year :int, month: str, response: Response):
     try:
         month_names = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december']
         month = month_names.index(month.lower())+1
         start_date = datetime(year, month, 1).date()
         end_date = datetime(year, month, number_of_days_in_month(year, month)).date()
     except ValueError:
-        return {
-            'code': 400,
-            'title': 'Bad Request',
-            'message': 'Invalid Month or year! Did you mispell the month or entered a wrong year?',
-            'full_help': 'Currently, the following names are valid inputs for month: ' + ', '.join(month_names) + '. Year has to be a positive integer.'
-        }
+        raiseException()
     
     URL = f'{BASE_URL}planetary/apod/?start_date={start_date}&&end_date={end_date}&&api_key={API_KEY}'
 
-    response = requests.get(URL,verify=False)
+    response = requests.get(URL)
     if(response.status_code == 200):
         result = response.json()
         result = [i['url'] for i in result if i['media_type'] == 'image']
         return result
     else:
-        return error_output,404
+        raiseException()
 
 
-@app.get('/nasa/videos-of-month/{year}/{month}')
-async def get_apod(year :int, month: str):
+@app.get('/nasa/videos-of-month/{year}/{month}', responses={404: {"model": ExceptionModel}})
+async def get_apod(year :int, month: str, response: Response):
     try:
         month_names = ['january', 'february', 'march', 'april', 'may', 'june', 'july', 'august', 'september', 'october', 'november', 'december']
         month = month_names.index(month.lower())+1
         start_date = datetime(year, month, 1).date()
         end_date = datetime(year, month, number_of_days_in_month(year, month)).date()
     except ValueError:
-        return {
-            'code': 400,
-            'title': 'Bad Request',
-            'message': 'Invalid Month or year! Did you mispell the month or entered a wrong year?',
-            'full_help': 'Currently, the following names are valid inputs for month: ' + ', '.join(month_names) + '. Year has to be a positive integer.'
-        }
+        raiseException()
     
     URL = f'{BASE_URL}planetary/apod/?start_date={start_date}&&end_date={end_date}&&api_key={API_KEY}'
 
-    response = requests.get(URL,verify=False)
+    response = requests.get(URL)
     if(response.status_code == 200):
         result = response.json()
         result = [i['url'] for i in result if i['media_type'] == 'video']
         return result
     else:
-        return error_output,404
+        raiseException()
 
 
-@app.get('/nasa/earth-poly-image/{date}')
-async def get_apod(date: str):
-    # Work in progress. filter by lat lon error.
+@app.get('/nasa/earth-poly-image/{date}', responses={404: {"model": ExceptionModel}})
+async def get_apod(date: str, response: Response):
     try:
         date_obj = datetime.strptime(date, '%Y-%m-%d').date()
     except ValueError:
-        return error_output,404
+        raiseException()
     
     URL = f'{BASE_URL}EPIC/api/natural/date/{date}?api_key={API_KEY}'
 
-    response = requests.get(URL,verify=False)
+    response = requests.get(URL)
     if(response.status_code == 200):
         results = response.json()
         results_list = []
@@ -107,4 +112,4 @@ async def get_apod(date: str):
         results_list = [i for i in results_list if (i['lat'] >= 10 and i['lat'] <= 40 and i['lon'] >= 120 and i['lon'] <= 160)]
         return results_list
     else:
-        return error_output,404
+        raiseException()
